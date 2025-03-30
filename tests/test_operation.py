91c001638dd20d87519926d9639bcefffa01eae3
Datkhoo25 <lucaskhoo98#gmail.com>
2025-03-30 17:58:47 +0800

    Unit Test Cases Updated

---
+++ tests/test_operation.py
@@ -0,0 +1,9 @@
+from appgitaction.src.math_operation import add, sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+
+def test_subtract():
+    assert sub(2,3)==-1
+    assert add(7,5)==2
