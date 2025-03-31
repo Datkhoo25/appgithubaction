c08495e697c5362d320665ea7a874824ea46c2e9
Datkhoo25 <lucaskhoo98#gmail.com>
2025-03-31 11:51:54 +0800

    Updated the unittest.yml

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -4,7 +4,7 @@ import os
 # Add the 'src' directory to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 
-from appgithubaction.src.math_operation import add, sub
+from src.math_operation import add, sub
 
 def test_add():
     assert add(2,3)==5
